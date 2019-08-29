191084d49cd61d083b4d80e2b9f930c8b38b078a
Chris Haining <chrishaining@yahoo.co.uk>
2019-08-29 07:56:37 +0100

    adds a few additional points to presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ member5.save()
 fitness_class1 = FitnessClass.new ( { 'name' => 'Pilates', 'datetime' => '2019-08-27 18:00:00', 'capacity' => 5 })
 fitness_class2 = FitnessClass.new ( { 'name' => 'Yoga', 'datetime' => '2019-08-28 18:00:00', 'capacity' => 5 })
 fitness_class3 = FitnessClass.new ( { 'name' => 'Killer Kardio', 'datetime' => '2019-08-29 14:00:00', 'capacity' => 10 })
-fitness_class4 = FitnessClass.new ( { 'name' => 'Codercise', 'datetime' => '2019-08-29 17:00:00', 'capacity' => 5 })
+fitness_class4 = FitnessClass.new ( { 'name' => 'Codercise', 'datetime' => '2019-08-29 17:00:00', 'capacity' => 1 })
 fitness_class5 = FitnessClass.new ( { 'name' => 'Powerpump', 'datetime' => '2019-08-28 08:00:00', 'capacity' => 5 })
 fitness_class6 = FitnessClass.new ( { 'name' => 'Cartwheels', 'datetime' => '2019-05-27 17:00:00', 'capacity' => 4 })
 
